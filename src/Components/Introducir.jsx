93c25ad100f88f9024f7e21eab1955af6d4838d0
Darío Chinea Delgado <dariocd0808@gmail.com>
2022-09-15 10:23:30 +0100

    Se añadió poder ver la lista, aunque solo se puede ver uno, y poder borrar, sin poder seleccionar

--- src/Components/Introducir.jsx
+++ src/Components/Introducir.jsx
@@ -1,3 +1,28 @@
-const Introducir = () => {
+import PropTypes from "prop-types";
+const Introducir = ({datosLista, setDatosLista}) => {
+    const Borrar = () =>{
+        setDatosLista({
+            cosa: ''
+        });
+    }
+    return (
+        <>
+        <h4>TaskList</h4>
+        <div>
+            <div className="MostrarLista">
+            {datosLista.cosa}
+            <p>
+                <button className="botonDelete" onClick={Borrar}>Delete all done</button>
+            </p>
+            </div>
+        </div>
+        </>
+    );
+};
+Introducir.propTypes ={
+    datosLista: PropTypes.object.isRequired
+    }
+    Introducir.propTypes ={
+    setDatosLista: PropTypes.func.isRequired
     }
 export default Introducir;
